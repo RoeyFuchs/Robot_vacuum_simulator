e5c589e70fc8a95a4158cc71db782b59c36a5df8
Stav Koren <stavkoren123@gmail.com>
2020-04-13 12:55:22 +0300

    Transfer Logger Parser, Pair, Report into new project

--- src/Main.java
+++ src/Main.java
@@ -29,13 +29,7 @@ public class Main {
         }
         logger.save();
 
-        var a=LoggerParser.GetReports("test2.xml");
-        for (Report r:a) {
-            System.out.println("Agent Loc: ("+r.getAgentLocation().getX()
-            +","+r.getAgentLocation().getY()+")");
-            System.out.println("Checked points: "+r.GetPointsChecked().size());
-            System.out.println("Compared points: "+r.GetPointsCompared().size());
-        }
+
 
 
 
