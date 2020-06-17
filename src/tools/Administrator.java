6bd4e2bdd7dbe570d06c927138136c9b512ccc46
fuchsroey <fuchsroey@gmail.com>
2020-06-17 13:42:40 +0300

    add comments

--- src/tools/Administrator.java
+++ src/tools/Administrator.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class Administrator {
     private Map map;
-    private List<Agent> agent = new LinkedList<>();
+    private List<Agent> agent = new LinkedList<>(); //can handle more than one agent
 
 
     public Administrator(Map map, Agent agent) {
@@ -19,7 +19,7 @@ public class Administrator {
     public void doOneStep() {
         for (Agent a : this.agent) {
             Point newPoint = a.doStep();
-            if (newPoint!= null &&this.map.legalMove(newPoint)) {
+            if (newPoint!= null &&this.map.legalMove(newPoint)) { //check if that what the agent wants to do is legal
                 Point oldPoint = a.getLocation();
                 this.map.agentMove(a, oldPoint, newPoint);
                 a.setLocation(newPoint);
